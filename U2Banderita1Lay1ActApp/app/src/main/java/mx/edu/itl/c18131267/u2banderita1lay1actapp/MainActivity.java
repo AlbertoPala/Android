3b2e26d9f91bd94abd7a5642d1048134b27f0786
Alberto Palacios <79346041+AlbertoPala@users.noreply.github.com>
2022-09-01 09:07:47 -0500

    Add files via upload
    primer trabajo android (01/09/2022)

---
+++ U2Banderita1Lay1ActApp/app/src/main/java/mx/edu/itl/c18131267/u2banderita1lay1actapp/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.itl.c18131267.u2banderita1lay1actapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
